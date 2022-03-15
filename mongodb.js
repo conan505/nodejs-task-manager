b40663f5a0fccb8ba0f6b2a5bf32c52aabb2183d
Rahul Garg <thisisrg.iitr@gmail.com>
2022-03-15 20:19:06 +0530

    demo for querying, updating and deleting data in mongodb

--- mongodb.js
+++ mongodb.js
@@ -1,6 +1,6 @@
 const mongodb = require("mongodb")
+const { MongoClient, ObjectId } = mongodb;
 
-const MongoClient = mongodb.MongoClient;
 
 const connectionUrl = "mongodb://127.0.0.1:27017"
 const databaseName = "task-manager"
@@ -11,42 +11,43 @@ MongoClient.connect(connectionUrl, (error, client) => {
     }
 
     const db = client.db(databaseName);
-    // db.collection('users').insertOne({
-    //     name: 'Rahul Garg',
-    //     age: '22'
-    // })
-    db.collection('users').insertMany([
-        {
-            name: 'John',
-            age: '25'
-        },
-        {
-            name: 'Alexa',
-            age: '23'
-        }
-    ], (error, result) => {
-        if (error) {
-            return console.log('Unable to insert documents!')
-        }
-        console.log(result.ops);
+
+    // for querying data
+
+    db.collection('tasks').findOne({ _id: new ObjectId("622f72b53173838ab136553c") }, (error, task) => {
+        if (error) return console.log(error);
+        console.log(task);
     })
-    db.collection('tasks').insertMany([
+    db.collection('tasks').find({ completed: true }).toArray((error, result) => {
+        console.log(result);
+    })
+
+    //for updating data (updateOne and updateMany)
+
+    db.collection('tasks').updateMany(
         {
-            description: 'Eat Donner',
             completed: true
         },
         {
-            description: 'learn Dance',
-            completed: true
-        },
-        {
-            description: 'complete project',
-            completed: false
+            $set: {
+                completed: false
+            }
         }
-    ], (error, result) => {
-        if (error) {
-            return console.log('Unable to insert documents!')
-        }
-        console.log(result.ops);
+    ).then((result) => {
+        console.log(result);
+    }).catch((error) => {
+        console.log(error);
     })
+
+
+    //for deleting data (deleteOne and deleteMany)
+    db.collection('users').deleteMany(
+        {
+            name: 'Rahul Garg'
+        }).then((result) => {
+            console.log(result);
+        }).catch((error) => {
+            console.log(error);
+        })
+
 })
