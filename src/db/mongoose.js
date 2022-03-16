259b537be14d9a7bf2eb5e99c6d9658d18961458
Rahul Garg <thisisrg.iitr@gmail.com>
2022-03-16 21:28:53 +0530

    add create and read APIs for users and tasks

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,44 +1,8 @@
 const mongoose = require('mongoose');
 
+
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api')
 
-const User = new mongoose.model('Users', {
-    name: {
-        type: String
-    },
-    age: {
-        type: Number
-    }
-})
 
-const me = new User({
-    name: 'Rahul Garg',
-    age: '22'
-})
 
-me.save().then(() => {
-    console.log(me);
-}).catch((error) => {
-    console.log(error);
-})
-
-const Task = new mongoose.model('Tasks', {
-    description: {
-        type: String
-    },
-    completed: {
-        type: Boolean
-    }
-})
-
-const work = new Task({
-    description: "Learn about hyperledger Fabric",
-    completed: false
-})
-
-work.save().then(() => {
-    console.log(work);
-}).catch((error) => {
-    console.log(error);
-})
 
---
+++ src/index.js
@@ -0,0 +1,85 @@
+const express = require('express');
+require('./db/mongoose')
+const Task = require('./models/tasks');
+const User = require('./models/users');
+
+const app = express();
+
+app.use(express.json());
+const port = process.env.PORT || 3000
+
+app.post('/users', (req, res) => {
+    const user = new User(req.body);
+
+    user.save().then(() => {
+        res.status(201).send(user);
+    }).catch((e) => {
+        res.status(400).send(e);
+    })
+})
+
+app.get('/users', (req, res) => {
+    User.find().then((users) => {
+        res.status(200).send(users);
+    }).catch((e) => {
+        res.status(500).send(e);
+    })
+})
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id;
+
+    User.findById(_id).then((user) => {
+        if (!user) return res.status(404)
+
+        res.status(200).send(user);
+    }).catch((e) => {
+        res.status(500).send(e);
+    })
+})
+
+app.post('/tasks', (req, res) => {
+    const task = new Task(req.body)
+
+    task.save().then(() => {
+        res.status(201).send(task);
+    }).catch((e) => {
+        res.status(400).send(e);
+    })
+})
+
+app.get('/tasks', (req, res) => {
+    Task.find().then((tasks) => {
+        res.status(200).send(tasks);
+    }).catch((e) => {
+        res.status(500).send(e);
+    })
+})
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id;
+
+    Task.findById(_id).then((task) => {
+        if (!task) return res.status(404)
+
+        res.status(200).send(task);
+    }).catch((e) => {
+        res.status(500).send(e);
+    })
+})
+
+app.listen(port, () => {
+    console.log('Server is up at ' + port)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
